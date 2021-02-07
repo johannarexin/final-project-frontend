38d36408514ad102d8d814da91698d62edee765f
JOHANNA REXIN <johanna.rexin@gmail.com>
2021-02-07 22:54:21 +0100

    All fetches work

--- src/components/Singleproduct.js
+++ src/components/Singleproduct.js
@@ -10,15 +10,27 @@
 
 import React, {useState, useEffect} from 'react'
 
-export const Singleproduct = () => {
-
-  useEffect(() => {}, [])
+export const Singleproduct = ({match}) => {
+  console.log(match)
+  useEffect(() => {
+    fetchCeramic()
+  }, [])
 
   const [ceramic, setCeramic] = useState({})
 
+  const fetchCeramic = async () => {
+    const fetchCeramic = await fetch (`https://final-project-backend-rexin.herokuapp.com/ceramics/${match.params.id}`)
+    const item = await fetchCeramic.json()
+    setCeramic(item)
+    console.log(item)
+  }
+
   return (
     <div>
-      <h1>KERAMIK DETALJER</h1>
+      <h1>{ceramic.header}</h1>
+      <p>{ceramic.text}</p>
+      <img src={ceramic.image} alt="" />
+
     </div>
   )
 }
