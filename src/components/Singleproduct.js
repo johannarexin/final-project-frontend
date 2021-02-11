84f3376a1d14fe2617c86f675e95c9210427a725
JOHANNA REXIN <johanna.rexin@gmail.com>
2021-02-11 23:42:55 +0100

    Fixed layout on single product page

--- src/components/Singleproduct.js
+++ src/components/Singleproduct.js
@@ -17,13 +17,9 @@ export const Singleproduct = ({match}) => {
 
   return (
     <div className="single-product-container">
-      <div className="single-product-text-container">
-        <h1 className="single-product-header">{ceramic.header}</h1>
-        <p className="single-product-text">{ceramic.text}</p>
-      </div>
-      <div>
         <img className="single-product-image" src={ceramic.image} alt="" />
-      </div>
+        <h1 className="single-product-header">{ceramic.header}</h1>
+        <p className="single-product-text">{ceramic.text}</p>      
     </div>
   )
 }
